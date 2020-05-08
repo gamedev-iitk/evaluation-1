1af5e7d0e79087cdc7128315aab6014bebd6ddb5
dxaman <niketdx@iitk.ac.in>
2020-05-08 19:08:10 +0530

    updated

---
+++ Assets/Scripts/Points.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Points : MonoBehaviour
+{
+    public int points=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnGUI()
+    {
+        GUI.Label(new Rect(20,20,250,100),"SCORE : "+ points);
+    }
+}
