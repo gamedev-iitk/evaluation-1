6ce6067efc9ad6f43fc5edb4bbc9beda022bf42b
heetesh <heetesh40@gmail.com>
2020-05-10 19:17:32 +0530

    heetesh

---
+++ JohnLemon/Assets/Scripts/Coin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coin : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        CoinCount.coinAmount += 1;
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ JohnLemon/Assets/Scripts/CoinCount.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCount : MonoBehaviour {
+
+    // Start is called before the first frame update
+    Text tex;
+     public static int coinAmount;
+    void Start()
+    {
+        tex = GetComponent<Text>();
+    }
+    void Update()
+    {
+        tex.text = coinAmount.ToString();
+    }
+
+
+}
