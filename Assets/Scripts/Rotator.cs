f4504bf175e149aa600aec771e41d9e07f93e0f7
omega64 <65087509+omega64@users.noreply.github.com>
2020-05-10 18:53:03 +0530

    final game by Rishabh Shah

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+  
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 90) * Time.deltaTime);
+    }
+}
