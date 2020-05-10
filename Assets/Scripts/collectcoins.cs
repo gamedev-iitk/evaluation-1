d9113ee501e0b890e453aa2f8ed66b4777cb09af
tanishq67 <tanishq67@gmail.com>
2020-05-10 23:36:56 +0530

    Game completed

---
+++ Assets/Scripts/collectcoins.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class collectcoins : MonoBehaviour
+{
+
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(90 * Time.deltaTime, 0, 0);
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == player)
+        {
+            other.GetComponent<PlayerMovement>().points++;
+            Destroy(gameObject);
+        }
+    }
+
+}
