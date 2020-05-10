a54e350b5b4aadc45a9814eaaf2bf997bd1fe40d
Shalvin Saksena <saksenas20@gmail.com>
2020-05-10 22:02:45 +0530

    Added Welcome Screen with play and quit button , Also Added Lemons For John To collect as he finds his way to safe zone

---
+++ John Lemon BY shalvin/Assets/Scripts/countscript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class countscript : MonoBehaviour
+{
+    public int lemons=0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnGUI()
+    {
+        GUI.Label(new Rect(100,100,1000,200),"Lemons Collected : "+lemons);
+    }
+}
