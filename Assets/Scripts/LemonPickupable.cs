6f3b1a8ab06f867cae9c7c74f0277dc3eae67b01
RajpootKn <65081780+RajpootKn@users.noreply.github.com>
2020-05-10 19:55:42 +0530

    adding game files

---
+++ Assets/Scripts/LemonPickupable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LemonPickupable : Pickupable
+{
+    protected override IEnumerator Pickup() 
+    {
+        yield return 1;
+    }
+}
---
+++ Assets/Scripts/Pickupable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Pickupable : MonoBehaviour
+{
+   
+    /// <summary>
+    /// Classes that extends from this
+    /// can specify their logic here.
+    /// </summary>
+    protected abstract IEnumerator Pickup();
+
+    /// <summary>
+    /// This function can be called from
+    /// the player to pickup this object.
+    /// </summary>
+    public void PickupObject()
+    {
+        Pickup();
+        Destroy(gameObject);
+    }
+}
