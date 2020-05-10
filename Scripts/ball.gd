b498ea536e5c99fdadb2efb27a2beeb6803aa04e
jatinyadav361 <jatinrao361@gmail.com>
2020-05-10 22:55:31 +0700

    Scripts

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -20,4 +20,14 @@ func _physics_process(delta):
 	
 	if get_position().y >  get_viewport_rect().end.y:
 		queue_free()
+		get_node("/root/world").score = 0
 		print("Ball died")
+
+var score = 0 setget set_score
+
+func set_score(value):
+	if get_position().y >  get_viewport_rect().end.y:
+		score = 0
+	else:
+		score = value
+	get_node("Label").set_text("Robots : "+str(score))
